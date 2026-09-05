a6232d089f9757b6e86f9ee6fcc881c973560aae
hashan-jay <hashanjayy@gmail.com>
2026-09-05 12:53:48 +1000

    Updated Version

--- frontend/src/components/PortalCredits.jsx
+++ frontend/src/components/PortalCredits.jsx
@@ -3,7 +3,7 @@ export default function PortalCredits({ className = '', employeePortal = false }
     return (
       <p className={['portal-credits', className].filter(Boolean).join(' ')}>
         <span>©2026 Port City BPO (Pvt) Ltd. All Rights Reserved</span>
-        <span>Employee Break Tracking System - Developer Version 5.2</span>
+        <span>Employee Break Tracking System | Developer Version 6.0 Beta</span>
       </p>
     );
   }
@@ -12,7 +12,7 @@ export default function PortalCredits({ className = '', employeePortal = false }
     <p className={['portal-credits', className].filter(Boolean).join(' ')}>
       <span>Port City BPO (Pvt) Ltd</span>
       <span>©2026 All Rights Reserved</span>
-      <span>Employee Break Tracking System - Version 5.2</span>
+      <span>Employee Break Tracking System | Version 6.0 Beta</span>
     </p>
   );
 }
